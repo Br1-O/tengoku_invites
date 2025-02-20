c50edce24d11cc45bb1910b47b5fff81690518b3
Br1-O <bruno.ortuno2@gmail.com>
2025-02-20 06:29:05 -0300

    Fix: Changes in token_invites file to work with env variables

--- lib/getTokenInviteValues.ts
+++ lib/getTokenInviteValues.ts
@@ -1,18 +1,11 @@
 "use server";
 
-import fs from "fs";
-import path from "path";
-
 function getTokenInvitesValues() {
   try {
-    const invitesFilePath = path.join(process.cwd(), "config/resources/", "token_invites.json");
-
-    if (!fs.existsSync(invitesFilePath)) {
-      console.error("Archivo token_invites.json no encontrado.");
-      return {};
+    let invites;
+    if (process.env.TOKEN_INVITES) {
+      invites = JSON.parse(process.env.TOKEN_INVITES);
     }
-
-    const invites = JSON.parse(fs.readFileSync(invitesFilePath, "utf8"));
     return invites;
   } catch (error) {
     console.error("Error al leer el archivo de invitaciones:", error);
