779473a55ea093c83de09e6604a6500e36d6c82d
Br1-O <bruno.ortuno2@gmail.com>
2025-03-06 04:12:41 -0300

    Fix: Changed style for announcement section

--- app/components/btns/socialMediaBar.tsx
+++ app/components/btns/socialMediaBar.tsx
@@ -5,23 +5,23 @@ const SocialMediaBar = () => {
   return (
     <div className={`${styles.card} flex flex-row justify-center items-center flex-wrap`}>
         <a className={`${styles.socialContainer} ${styles.containerFour}`} href="mailto: tengokuimperial@gmail.com" title='¡Mandanos un email!' aria-label='icono email'>
-            <MailIcon className={`w-8 h-8 ${styles.socialIcon}`}/>
+            <MailIcon className={`w-8 h-8 text-white ${styles.socialIcon}`}/>
         </a>
 
         <a className={`${styles.socialContainer} ${styles.containerOne}`} href="https://www.instagram.com/tengokuanime" title='¡Visitanos en Instagram!' aria-label='icono instagram'>
-            <InstagramIcon className={`w-8 h-8 ${styles.socialIcon}`} />        
+            <InstagramIcon className={`w-8 h-8 text-white ${styles.socialIcon}`} />        
         </a>
         
         <a className={`${styles.socialContainer} ${styles.containerTwo}`} href="https://www.facebook.com/ConvencionTengoku" title='¡Visitanos en Facebook!' aria-label='icono facebook'>
-            <FacebookIcon className={`w-8 h-8 ${styles.socialIcon}`} />
+            <FacebookIcon className={`w-8 h-8 text-white ${styles.socialIcon}`} />
         </a>          
 
         <a className={`${styles.socialContainer} ${styles.containerThree}`} href="https://www.youtube.com/@Tengokuimperial23" title='¡Mirá nuestros videos!' aria-label='icono youtube'>
-            <YoutubeIcon className={`w-8 h-8 ${styles.socialIcon}`} />
+            <YoutubeIcon className={`w-8 h-8 text-white ${styles.socialIcon}`} />
         </a>
 
         <a className={`${styles.socialContainer} ${styles.containerFour}`} href="mailto: tengoku.imperial.games@gmail.com" title='¡Mandanos un email!' aria-label='icono email'>
-            <MailIcon className={`w-8 h-8 ${styles.socialIcon}`} />
+            <MailIcon className={`w-8 h-8 text-white ${styles.socialIcon}`} />
         </a>
     </div>             
   );
